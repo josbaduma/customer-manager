4ae4370ee84fdb06fdb13ea20d64e6889b1894f9
Jose Badilla <josebaduma@gmail.com>
2026-07-05 19:44:33 -0600

    fix: update import path for PrismaClient to use alias

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const adapter = new PrismaBetterSqlite3({
   url: "./database/data.sqlite",
