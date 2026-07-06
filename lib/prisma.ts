ec202f74ee484e44fad2455834dbc6262305eab5
Jose Badilla <josebaduma@gmail.com>
2026-07-05 21:17:03 -0600

    chore: update dependencies and configure datasource URL for Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@/app/generated/prisma/client";
 
-const adapter = new PrismaBetterSqlite3({
-  url: "./database/data.sqlite",
+const adapter = new PrismaLibSql({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
